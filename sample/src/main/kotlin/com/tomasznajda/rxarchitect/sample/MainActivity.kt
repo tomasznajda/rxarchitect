b6be04d036de0a5ef37a6344b620c733efe23165
Tomasz <tomasz.najda@indoorway.com>
2018-12-19 20:09:46 +0100

    [sample] fix: deps & imports

--- sample/src/main/kotlin/com/tomasznajda/rxarchitect/sample/MainActivity.kt
+++ sample/src/main/kotlin/com/tomasznajda/rxarchitect/sample/MainActivity.kt
@@ -2,7 +2,6 @@ package com.tomasznajda.rxarchitect.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.tomasznajda.rxarchitect.R
 
 class MainActivity : AppCompatActivity() {
 
